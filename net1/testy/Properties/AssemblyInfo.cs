fdbbaf22b64e4bdd520bd7628b929abe472aed90
Adrian Sobecki <adriansobecki@onet.pl>
2021-03-10 14:19:11 +0100

    testy

---
+++ net1/testy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testy")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10504184-2f30-405f-beb8-82764a80c240")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ net1/testy/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using net1;
+namespace testy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            int x = rng.nextInt(1, 29);
+            Assert.IsTrue(x >= 1);
+        }
+    }
+}
