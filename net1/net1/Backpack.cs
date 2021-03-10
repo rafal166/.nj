3a3b4c1d149aade0d9ecc876b7204399d8c04d20
Rafał Rzewucki <rafal.rzewucki@gmail.com>
2021-03-10 14:30:21 +0100

    Dodanie plecaka

---
+++ net1/net1/Backpack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net1
+{
+    class Backpack
+    {
+        private int size;
+        private int remainingSize;
+
+        Backpack(int newSize) {
+            size = newSize;
+            remainingSize = newSize;
+        }
+        public void addNewItem(int itemSize) {
+            remainingSize -= itemSize;
+        }
+
+        public int getSize()
+        {
+            return size;
+        }
+        public int getRemainingSize()
+        {
+            return remainingSize;
+        }
+
+
+    }
+}
