ba2699339c7352a6081f29a8d2a40ef32eb51ef3
Adrian Sobecki <adriansobecki@onet.pl>
2021-03-10 14:12:50 +0100

    początek

---
+++ net1/net1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+        }
+    }
+}
